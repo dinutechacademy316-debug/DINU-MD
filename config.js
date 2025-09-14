ba9df3e7f62167da6f6dcf7a33c8c1d6e634ecdb
dinutechacademy316-debug <dinutechacademy316@gmail.com>
2025-09-14 05:28:24 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
-MONGODB: process.env.MONGODB || "mongodb://mongo:sjjkkd",  //ඔයාගෙ mongoDb url එක
+SESSION_ID: process.env.SESSION_ID || "CQsATADA#NeT-4Q1Mmhrg1qYK2uiUv5zUO2lyIDBks8LTicMihK4", // ඔයාගෙ session id එක
+MONGODB: process.env.MONGODB || "mongodb://mongo:fpwedYjILKVcmiXkLRJzWqiUzeZdInJl@switchback.proxy.rlwy.net:39273",  //ඔයාගෙ mongoDb url එක
 };
  
